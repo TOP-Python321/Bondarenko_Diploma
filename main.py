b59526e0a2b29c94f9fe24df656f581280dac2a6
Nikolai Bondarenko <dnobless@mail.ru>
2023-12-16 23:10:55 +0300

    initial 2

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
+from config_data.config import BOT_TOKEN
+
+
 from aiogram import Bot, Dispatcher
+
+from aiogram.filters import Command, CommandStart
+from aiogram.types import Message
+
+
+bot = Bot(token= BOT_TOKEN)
+
+dp = Dispatcher()
+
+@dp.message(CommandStart)
+async def command_start(message : Message):
+    await message.answer('Работает')
+
+
+
+
+
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
 
